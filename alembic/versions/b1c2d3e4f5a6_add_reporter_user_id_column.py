53879b806a8d19ca2065d335e01a077132ada82e
Syed Zubair <zubair.syed@vpdtechnologies.com>
2026-09-01 11:07:34 +0530

    feat(migration): add b1c2d3e4f5a6 linear head migration for reporter_user_id on Render

--- alembic/versions/b1c2d3e4f5a6_add_reporter_user_id_column.py
+++ alembic/versions/b1c2d3e4f5a6_add_reporter_user_id_column.py
@@ -1,20 +1,21 @@
-"""add_reporter_user_id
+"""add reporter_user_id column to rescue_requests
 
-Revision ID: 89d29c6bbfe2
-Revises: b2c3d4e5f6g7
-Create Date: 2026-08-25 15:00:00.000000
+Revision ID: b1c2d3e4f5a6
+Revises: a8b9c0d1e2f3
+Create Date: 2026-09-01 11:06:00.000000
 
 """
-
-from collections.abc import Sequence
+from typing import Sequence, Union
 
 from alembic import op
 import sqlalchemy as sa
 
-revision: str = "89d29c6bbfe2"
-down_revision: str | None = "b2c3d4e5f6g7"
-branch_labels: str | Sequence[str] | None = None
-depends_on: str | Sequence[str] | None = None
+
+# revision identifiers, used by Alembic.
+revision: str = "b1c2d3e4f5a6"
+down_revision: Union[str, None] = "a8b9c0d1e2f3"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
